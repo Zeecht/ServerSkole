2e29b6ef03d39db4cc1d826a233ec899442205fa
Zeech <Morten_hansen51@yahoo.dk>
2018-11-02 07:44:21 +0100

    f

---
+++ Server(studie)/Server_DrawServer/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server_DrawServer
+{
+    public partial class Form1 : Form
+    {
+        public bool isDrawing = true;
+        public List<Point> drawPosition = new List<Point>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            panel1.MouseDown += new MouseEventHandler(Mouse);
+        }
+
+
+        public void Mouse(object sender, MouseEventArgs me)
+        {
+
+            if (isDrawing)
+            {
+                drawPosition.Add(me.Location);
+                panel1.Invalidate();
+            }
+        }
+
+
+        public void Draw(object sender, PaintEventArgs pe)
+        {
+
+            //pe.Graphics.DrawLines(Pens.Black, drawingPosition);
+            panel1.Invalidate();
+        }
+
+
+    }
+}
